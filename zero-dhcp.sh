d18dd1f1b060d6ae0b295c78ef60de3aff4e81d2
Arnulf Heimsbakk <arnulf.heimsbakk@gmail.com>
2018-03-19 19:02:42 +0100

    fixed error in dnsmasq configuration

--- zero-dhcp.sh
+++ zero-dhcp.sh
@@ -52,7 +52,7 @@ dhcp-option=option:router, 172.31.7.1
 dhcp-option=121,0.0.0.0/1, 172.31.7.1, 128.0.0.0/1, 172.31.7.1
 dhcp-option=249,0.0.0.0/1, 172.31.7.1, 128.0.0.0/1, 172.31.7.1
 dhcp-option=vendor:MSFT, 2, 1i
-dhcp-option=dns-server, 172.31.7.1
+dhcp-option=option:dns-server, 172.31.7.1
 dhcp-option=option6:dns-server, [::]
 EOF
 
